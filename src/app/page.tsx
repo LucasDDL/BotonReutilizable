aceb2e8c6d6bf5a5706c5d232d790d1a630e75ff
Lucas Caballero <dante.dylangg@gmail.com>
2024-05-06 19:52:49 -0300

    margin auto de toda la vida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import { Settings } from "@/icons/components/Settings";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 mx-auto">
       <h1 className="text-primary-50 text-[20px] leading-[42px]">Clientes / <span className="text-[32px] leading-[42px]">Sebastian Salines</span></h1>
-      <div className="flex flex-col gap-8">
-        <Input startIcon={<Settings />} endIcon={<ArrowDown />} label={'Tipo'} />
+      <div className="flex flex-col gap-8 items-center">
+        <div className="self-start">
+          <Input startIcon={<Settings />} endIcon={<ArrowDown />} label={'Tipo'} />
+        </div>
         <div className="datos flex flex-col gap-8">
           <div className="flex flex-col gap-3">
             <h2 className="text-primary-40">Datos</h2>
@@ -41,7 +43,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex justify-end">
+        <div className="flex justify-end gap-6 pr-2 self-end">
           <Button variant="text" color="primary" size="md">Cancelar</Button>
           <Button disabled size="md">Guardar</Button>
         </div>
